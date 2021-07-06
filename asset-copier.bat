cbf72fce9b721c9676ae48350e1941589b097763
Nebual <ben1120@gmail.com>
2021-07-06 03:41:20 -0700

    Add readme

--- asset-copier.bat
+++
@@ -1,7 +0,0 @@
-xcopy /E /Y entity\ ..\..\..\entity\
-xcopy /E /Y materials\ ..\..\..\materials\
-xcopy /E /Y models\ ..\..\..\models\
-xcopy /E /Y particles\ ..\..\..\particles\
-xcopy /E /Y sounds\ ..\..\..\sounds\
-rmdir /q /s ..\..\..\code\ui\sandbox\
-xcopy /E /Y code\ui\sandbox\ ..\..\..\code\ui\sandbox\
