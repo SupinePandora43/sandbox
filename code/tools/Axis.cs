1426230fd9826b207d5f2843cc4f383cf039ac81
SupinePandora43 <supinepandoraggg@gmail.com>
2021-08-07 22:04:05 +0500

    Use PhysicsBody (ragdoll support) remove unused code

--- code/tools/Axis.cs
+++ code/tools/Axis.cs
@@ -3,11 +3,11 @@ namespace Sandbox.Tools
 	[Library( "tool_axis", Title = "Axis", Description = "Physical Axis" )]
 	public partial class AxisTool : BaseTool
 	{
-		private ModelEntity ent1, ent2;
+		private PhysicsBody body1, body2;
 
-		private Vector3 LNorm1, LNorm2;
+		private Vector3 LNorm1;
 		private Vector3 WNorm1, WNorm2;
-		private Vector3 LPos1, LPos2;
+		private Vector3 LPos1;
 		private Vector3 WPos1, WPos2;
 
 		private enum State
@@ -40,28 +40,28 @@ namespace Sandbox.Tools
 
 				if ( !tr.Entity.IsValid() )
 					return;
-
+				
 				if ( !(tr.Body.IsValid() && (tr.Body.PhysicsGroup != null) && tr.Body.Entity.IsValid()) ) return;
 				
-				if ( state is State.GET_ATTACHABLE || !ent1.IsValid() )
+				if ( state is State.GET_ATTACHABLE || !body1.IsValid() )
 				{
 					if ( tr.Entity.IsWorld || tr.Entity is WorldEntity ) return;
 
-					ent1 = (ModelEntity)tr.Entity;
+					body1 = tr.Body;
 
 					WNorm1 = tr.Normal;
-					LNorm1 = ent1.Transform.NormalToLocal( WNorm1 );
+					LNorm1 = body1.Transform.NormalToLocal( WNorm1 );
 
 					WPos1 = tr.EndPos;
-					LPos1 = ent1.Transform.PointToLocal( WPos1 );
+					LPos1 = body1.Transform.PointToLocal( WPos1 );
 
 					state = State.GET_ATTACH_TO_POINT;
 				}
 				else
 				{
-					ent2 = (ModelEntity)tr.Entity;
+					body2 = tr.Body;
 
-					if ( (!ent1.IsValid()) || ent1 == ent2 )
+					if ( (!body1.IsValid()) || body1 == body2 )
 					{
 						state = State.GET_ATTACHABLE;
 						return;
@@ -69,33 +69,32 @@ namespace Sandbox.Tools
 
 					#region Rotation
 
-					WNorm1 = ent1.Transform.NormalToWorld( LNorm1 );
+					WNorm1 = body1.Transform.NormalToWorld( LNorm1 );
 					WNorm2 = tr.Normal;
-					LNorm2 = ent2.Transform.NormalToLocal( WNorm2 );
 
-					ent1.Rotation = Rotation.LookAt( WNorm2 ) * Rotation.From( new Angles( 0, -180, 0 ) );
-					ent1.LocalRotation = Rotation.Difference( Rotation.LookAt( LNorm1 ), ent1.Rotation );
+					body1.Rotation = Rotation.LookAt( WNorm2 ) * Rotation.From( new Angles( 0, -180, 0 ) );
+					body1.Rotation = Rotation.Difference( Rotation.LookAt( LNorm1 ), body1.Rotation );
 
 					#endregion Rotation
 
 					#region Position
 
-					WPos1 = ent1.Transform.PointToWorld( LPos1 );
+					WPos1 = body1.Transform.PointToWorld( LPos1 );
 					WPos2 = tr.EndPos;
 
-					ent1.Position += WPos2 - WPos1;
+					body1.Position += WPos2 - WPos1;
 
 					#endregion Position
 
 					PhysicsJoint.Revolute
-						.From( ent1.IsWorld ? PhysicsWorld.WorldBody : ent1.PhysicsBody )
-						.To( ent2.IsWorld ? PhysicsWorld.WorldBody : ent2.PhysicsBody )
+						.From( body1.Entity.IsWorld ? PhysicsWorld.WorldBody : body1 )
+						.To( body2.Entity.IsWorld ? PhysicsWorld.WorldBody : body2 )
 						.WithPivot( tr.EndPos )
 						.WithBasis( Rotation.LookAt( tr.Normal ) * Rotation.From( new Angles( 90, 0, 0 ) ) )
 						.Create();
 
-					ent1.PhysicsGroup?.Wake();
-					ent2.PhysicsGroup?.Wake();
+					body1.PhysicsGroup?.Wake();
+					body2.PhysicsGroup?.Wake();
 
 					state = State.GET_ATTACHABLE;
 				}
